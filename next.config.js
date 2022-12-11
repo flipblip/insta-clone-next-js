d731c86842383efa7b7f9756f1005b2f89a228cb
fliblip <wickynjy@gmail.com>
2022-12-11 17:23:11 +0300

    Completed Header

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["links.papareact.com"],
+  }
 }
+
+// module.exports = {
+  
+// }
 
 module.exports = nextConfig
