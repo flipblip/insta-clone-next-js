0f418c42dbc8de443be2be430e798626ef395456
thrasher <wickynjy@gmail.com>
2022-12-21 01:49:59 -0600

    add fake posts data, import props in posts.js

---
+++ pages/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post({ id, username, userImg, img, caption}) {
+  return (
+    <div>
+        <h1>I am a post</h1>
+    </div>
+  )
+}
+
+export default Post
--- pages/components/Posts.js
+++ pages/components/Posts.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import Post from './Post'
 
-const Posts = () => {
+const posts =[
+    {
+        id: '123',
+        username: 'Wicky',
+        userImg: 'https://links.papareact.com/3ke',
+        img: 'https://links.papareact.com/3ke',
+        caption: "SEE YOU IN 2023",
+    },
+    {
+        id: '123',
+        username: 'Wicky',
+        userImg: 'https://links.papareact.com/3ke',
+        img: 'https://links.papareact.com/3ke',
+        caption: "SEE YOU IN 2023",
+    },
+]
+
+function Posts() {
   return (
     <div>
-        
+        {posts.map((post) =>(
+            <Post key={post.id} id={post.id}
+            username={post.username}
+            userImg={post.userImg}
+            img={post.img}
+            caption={post.caption}
+
+            />
+        ))}
+
     </div>
   )
 }
